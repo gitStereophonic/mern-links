9d996dca4b6ebabfe6a865990b5d0351b17c9235
Sergey Chinkov <sergey.chinkov@yandex.ru>
2021-04-01 19:42:34 +0300

    [Fix] Fixed create button

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -13,19 +13,22 @@ const CreatePage = () => {
   const { request } = useHttp();
   const [link, setLink] = useState('');
   const handleChange = (e) => setLink(e.target.value);
+  const handleClick = async () => {
+    try {
+      const data = await request(
+        '/api/link/generate',
+        'POST',
+        {
+          from: link,
+        },
+        { Authorization: `Bearer ${auth.token}` }
+      );
+      history.push(`/detail/${data.link._id}`);
+    } catch (e) {}
+  };
   const handleKeyPress = async (e) => {
     if (e.key === 'Enter') {
-      try {
-        const data = await request(
-          '/api/link/generate',
-          'POST',
-          {
-            from: link,
-          },
-          { Authorization: `Bearer ${auth.token}` }
-        );
-        history.push(`/detail/${data.link._id}`);
-      } catch (e) {}
+      handleClick();
     }
   };
 
@@ -51,6 +54,7 @@ const CreatePage = () => {
           className='btn waves-effect waves-light green'
           type='button'
           name='action'
+          onClick={handleClick}
         >
           Создать
           <i className='material-icons right'>send</i>
